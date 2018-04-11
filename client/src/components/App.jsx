ae66154014ac13d190eb8edfa33e1759e30753dd
POURSH <poursh@POURSHs-MacBook-Pro.local>
2018-04-10 17:06:37 -0700

    made a preliminary client

---
+++ client/src/components/App.jsx
@@ -0,0 +1,75 @@
+import React from 'react';
+
+import sample from './sample_data'
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      //
+    }
+  }
+
+  // changeView(option) {
+  //   this.setState({
+  //     view: option
+  //   })
+  // }
+
+  // renderView() {
+  //   const {view} = this.state;
+
+  //   if (view === 'feed') {
+  //     return <Feed handleClick={(() => this.changeView(view)) } data={this.state.data} view={this.state.view}/>
+  //   } else if (view === 'admin') {
+  //     return <Admin data={this.state.data} />
+  //   } else if (view === 'create') {
+  //     return <Create data={this.state.data} />
+  //   } else {
+  //     return <Post key={view._id} post={view} />
+  //   }
+  // }
+  
+  render() {
+    return (
+      <div>
+        <div className="nav">
+          <span className="logo"
+            onClick={() => this.changeView('feed')}>
+            Photostory
+          </span>
+          <span className={
+            this.state.view === 'feed'
+            ? 'nav-selected'
+            : 'nav-unselected'}
+            onClick={() => this.changeView('feed')}>
+            See all Posts
+          </span>
+          {/* <span className="nav-unselected">
+            Write a Post
+          </span>
+          <span className={
+            this.state.view === 'admin'
+            ? 'nav-selected'
+            : 'nav-unselected'}
+            onClick={() => this.changeView('admin')}>
+            Admin
+          </span>
+          <span className={
+            this.state.view === 'create'
+            ? 'nav-selected'
+            : 'nav-unselected'}
+            onClick={() => this.changeView('create')}>
+            Write a Post
+          </span> */}
+        </div>
+
+        <div className="main">
+          {this.renderView()}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
