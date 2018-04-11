1fad0a0e9c36b7b4df2634a613b3d9dd08475da5
jaba9531 <jakecbarber@gmail.com>
2018-04-10 19:44:40 -0700

    add DB index and tables

---
+++ DB/index.js
@@ -0,0 +1,21 @@
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  database: "photostory"
+});
+
+connection.connect(function(err) {
+  if (err) {
+    throw err;
+  }
+  console.log("Connected!");
+});
+
+module.exports = connection;
+
+
+
+
+
