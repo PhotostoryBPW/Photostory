de6f931f866f1568f83840d44a618be880bfa94c
jaba9531 <jakecbarber@gmail.com>
2018-04-13 15:00:25 -0700

    login and signup are working(passwords not hashed)

--- server/router/index.js
+++ server/router/index.js
@@ -4,11 +4,14 @@ const Controller = require('../Controller');
 
 router.get('/feed', Controller.posts.all);
 
-router.post('/post', Controller.posts.create);
+// router.post('/post', Controller.posts.create);
 
 // router.get('/find', Controller.Users.Find);
 
-// router.post('/signup', Controller.Users.Signup);
+router.post('/signup', (req, res) => {
+    console.log('hello from router signup');
+    Controller.Signup(req, res);
+});
 
 router.get('/login', Controller.Login);
 
